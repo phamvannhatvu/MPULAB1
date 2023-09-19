077a39f1d765922762c72e9d8a8ceb75c2a58222
phamvannhatvu <vu.pham2512@hcmut.edu.vn>
2023-09-19 13:21:20 +0700

    Add exercise 9

--- AnalogClock/Core/Src/main.c
+++ AnalogClock/Core/Src/main.c
@@ -51,6 +51,7 @@ static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 void clearAllClock();
 void setNumberOnClock(int);
+void clearNumberOnClock(int);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -94,15 +95,15 @@ int main(void)
   uint8_t count = 0;
   while (1)
   {
-	  switch (count)
+	  if (count < 12)
 	  {
-	  case 12:
-		  count = 12;
-		  clearAllClock();
-		  break;
-	  default:
 		  setNumberOnClock(count);
-		  break;
+	  }else if (count < 24)
+	  {
+		  clearNumberOnClock(count - 12);
+	  }else
+	  {
+		  count = 0;
 	  }
 	  ++count;
 	  HAL_Delay(1000);
@@ -239,6 +240,49 @@ void setNumberOnClock(int num)
 		break;
 	case 11:
 		HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, GPIO_PIN_SET);
+		break;
+	}
+}
+
+void clearNumberOnClock(int num)
+{
+	switch (num)
+	{
+	case 0:
+		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+		break;
+	case 1:
+		HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+		break;
+	case 2:
+		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_RESET);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, GPIO_PIN_RESET);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, GPIO_PIN_RESET);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, GPIO_PIN_RESET);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, GPIO_PIN_RESET);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, GPIO_PIN_RESET);
+		break;
+	case 8:
+		HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, GPIO_PIN_RESET);
+		break;
+	case 9:
+		HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, GPIO_PIN_RESET);
+		break;
+	case 10:
+		HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, GPIO_PIN_RESET);
+		break;
+	case 11:
+		HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, GPIO_PIN_RESET);
 		break;
 	}
 }
